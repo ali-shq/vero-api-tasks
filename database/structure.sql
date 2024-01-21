eae17b24c33d44e0bd3e5da8aa4043938f35dbff
Ali <kledisali88@gmail.com>
2024-01-21 13:38:25 +0100

    changed columns to snake case

--- database/structure.sql
+++ database/structure.sql
@@ -1,12 +1,12 @@
 create table if not exists construction_stages
 (
-    ID           integer primary key,
-    name         varchar(255)              not null,
-    start_date   datetime                  not null,
-    end_date     datetime,
-    duration     float,
-    durationUnit varchar(50),
-    color        varchar(50),
-    externalId   nvarchar(255),
-    status       varchar(50) default 'NEW' not null
+    id            integer primary key,
+    name          varchar(255)              not null,
+    start_date    datetime                  not null,
+    end_date      datetime,
+    duration      float,
+    duration_unit varchar(50),
+    color         varchar(50),
+    external_id   nvarchar(255),
+    status        varchar(50) default 'NEW' not null
 )
